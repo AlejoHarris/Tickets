33c7fa12c2594254477674400ac7a5d247f27d09
Alejandro Harris <alejandro@getfaveo.co>
2021-09-18 07:56:47 +0200

    SignIn and SignUp Components added

--- ticketfront/src/App.js
+++ ticketfront/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import SignIn from "./components/signIn.component";
+import SignUp from "./components/signUp.component";
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <h3>Build Sign Up & Login UI Template in React</h3>
-    </div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>Tickets-Challenge</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Sign In</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={SignIn} />
+            <Route path="/sign-in" component={SignIn} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
